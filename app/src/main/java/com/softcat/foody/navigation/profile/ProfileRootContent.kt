5ca4268835fadbeda934ea9c5c7878514bd77e26
d_machilsky <d.machilsky@pay-lab.ru>
2026-09-09 21:20:44 +0300

    Cleaning code.

--- app/src/main/java/com/softcat/foody/navigation/profile/ProfileRootContent.kt
+++ app/src/main/java/com/softcat/foody/navigation/profile/ProfileRootContent.kt
@@ -2,7 +2,7 @@ package com.softcat.foody.navigation.profile
 
 import androidx.compose.runtime.Composable
 import com.arkivanov.decompose.extensions.compose.stack.Children
-import com.softcat.foody.screens.authorization.AuthContent
+import com.softcat.foody.screens.authorization.AuthScreen
 import com.softcat.foody.screens.profile.ProfileScreen
 import com.softcat.foody.screens.scores.ScoresScreen
 
@@ -10,7 +10,7 @@ import com.softcat.foody.screens.scores.ScoresScreen
 fun ProfileRootContent(component: ProfileRoot) {
     Children(component.stack) {
         when (val instance = it.instance) {
-            is ProfileRoot.Child.Authorization -> AuthContent(instance.component)
+            is ProfileRoot.Child.Authorization -> AuthScreen(instance.component)
             is ProfileRoot.Child.Profile -> ProfileScreen(instance.component)
             is ProfileRoot.Child.Scores -> ScoresScreen(instance.component)
         }
